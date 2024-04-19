dc19eb42859d6b170277d4a46f3cce2449568384
ivan-shestakov-qa <git@github.com:ivan-shestakov-qa/Java_Spring.git>
2024-04-19 10:43:02 +0300

    lesson4

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -9,10 +9,21 @@ public class TestSpring {
         );
 //        Music music = context.getBean("musicBean", Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comparison = musicPlayer1 == musicPlayer2;
+        System.out.println(comparison);
+
+        System.out.println(musicPlayer1);
+        System.out.println(musicPlayer2);
+
+        musicPlayer2.setVolume(25);
+        System.out.println(musicPlayer1.getVolume());
+        System.out.println(musicPlayer2.getVolume());
+//        musicPlayer.playMusic();
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
         context.close();
     }
 }
