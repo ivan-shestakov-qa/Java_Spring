90718b0803d0f9698fe258b00e7f59f5df10d7ab
ivan-shestakov-qa <git@github.com:ivan-shestakov-qa/Java_Spring.git>
2024-04-19 09:51:54 +0300

    lesson4

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,10 +1,18 @@
 package org.example;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
     public void playMusic(){
         System.out.println("playing: " + music.getSong());
     }
