4139e7aa8bff54a75859a394303d7521500df5fa
ivan-shestakov-qa <git@github.com:ivan-shestakov-qa/Java_Spring.git>
2024-04-22 21:52:18 +0300

    lesson7

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -3,6 +3,7 @@ package org.example;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,16 +12,16 @@ import org.springframework.stereotype.Component;
 @Getter
 @Setter
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
+    private Music music1;
+    private Music music2;
 //    private String name;
 //    private int volume;
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic){
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer(@Qualifier("rockMusic") Music music1, @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
     public String playMusic(){
-        return "playing: " + classicalMusic.getSong();
+        return "playing: " + music1.getSong() + ", " + music2.getSong();
     }
 }
