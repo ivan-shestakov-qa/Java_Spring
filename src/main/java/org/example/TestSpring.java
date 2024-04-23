a33b7c2c0b4859710b3ec3727b7dde717b650841
ivan-shestakov-qa <git@github.com:ivan-shestakov-qa/Java_Spring.git>
2024-04-23 18:28:53 +0300

    lesson9

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -1,12 +1,14 @@
 package org.example;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
-        );
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+//                "applicationContext.xml"
+//        );
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 //        Computer computer = context.getBean("computer", Computer.class);
 //        System.out.println(computer);
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
